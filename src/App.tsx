2069f675cf0b30d5ef97461514e8e935cf57fc34
Aiden <aiden@oldstreetjournal.co.uk>
2021-06-21 20:52:47 +0100

    fullscreen

--- src/App.tsx
+++ src/App.tsx
@@ -16,22 +16,17 @@ const keymap: Keymapping = {
 
 const Atom = () => <img src={logo} className="App-logo" alt="logo" />;
 const Circle = () => <div style={{backgroundColor: 'red', height:"50px", width:"50px", borderRadius: "25px"}}></div>
-const shapeTrigger = () => {
-  {(e: KeyboardEvent) => {
-    
-  }} 
-}
 function App() {
   const [atomVisibile, setAtomVisibile] = React.useState(false);
   return (
-    <div style={{width: "100%", height:"100%"}}
+    <div id='app' style={{width: "100%", height:"100%"}}
       tabIndex={1}
-      onKeyUp={(e) => {
+      onKeyUp={(e: { key: string; }) => {
         if (e.key === "q") {
           setAtomVisibile(false);
         }
       }}
-      onKeyDown={(e) => {
+      onKeyDown={(e: { key: string; }) => {
         console.log('a button was pressed')
         if (e.key === "q") {
         console.log('it was q')
