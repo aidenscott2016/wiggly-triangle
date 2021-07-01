4410eaecce047cfdabc36550256edb60bf5cc6dc
Aiden Scott <aiden.scott@ovoenergy.com>
2021-07-01 20:09:39 +0100

    create time elaspsed hook

--- src/utils/KeyPresser.tsx
+++ src/utils/KeyPresser.tsx
@@ -7,13 +7,37 @@ type Mapping = { key: string; component: (props: IsVisible) => JSX.Element };
 type Props = {
   mappings: Mapping[];
 };
+
+type Seconds = number;
+type Key = string;
+type Note = [Seconds, Key];
+type Timeline = Note[];
+
 export const KeyPresser = ({ mappings }: Props) => {
+  const useTimeElapsed = () => {
+    const [startTime, setStartTime] = React.useState(0);
+    React.useEffect(() => {
+      setStartTime(performance.now());
+    }, []);
+    return () => performance.now() - startTime;
+  };
+
   const [pressedKeys, updatePressedKeys] = React.useState({});
+  const [timeline, updateTimeline] = React.useState<Timeline>([]);
+  const timeElapsed = useTimeElapsed()
+
+  const addToTimeline = (e: Event) => {
+    updateTimeline([...timeline]);
+  };
+
+  const clearTimeline = () => updateTimeline([]);
+
   React.useEffect(() => {
     document.title = JSON.stringify(pressedKeys);
   }, [pressedKeys]);
 
   const handleKeyDown: React.KeyboardEventHandler = (e) => {
+    console.log(timeElapsed())
     updatePressedKeys({ ...pressedKeys, [e.key.toLowerCase()]: true });
   };
   const handleKeyUp: React.KeyboardEventHandler = (e) => {
