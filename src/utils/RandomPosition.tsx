9797ff4bb11c930a48697a3b2cbb718ac7d54aed
Aiden Scott <aiden.scott@ovoenergy.com>
2021-07-01 19:15:36 +0100

    m

--- src/utils/RandomPosition.tsx
+++ src/utils/RandomPosition.tsx
@@ -1,8 +1,4 @@
-import { useLayoutEffect } from "react";
-import { useRef } from "react";
 import { SizeMeProps, withSize } from "react-sizeme";
-import { useRandomPosition } from "../hooks/useRandomPosition";
-import { randomPos } from "../util";
 
 // export const RandomPosition = ({ children }: React.PropsWithChildren<{}>) => {
 //   return (
@@ -16,15 +12,15 @@ export const RandomPositionContainer = ({
   size,
   children,
 }: React.PropsWithChildren<SizeMeProps>) => {
-    console.log("size", size)
-    useLayoutEffect <- ref to the elem
-    const random = useRandomPosition(size.height, size.width)
-    console.log("location", random)
+    // console.log("size", size)
+    // // useLayoutEffect <- ref to the elem
+    // const random = useRandomPosition(size.height, size.width)
+    // console.log("location", random)
   return (
     <div
       id="rando"
       style={{
-        ...random,
+        // ...random,
         position: "absolute",
       }}
     >
