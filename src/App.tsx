702e5d52d578a0b20aa2cae2356d5ae922e4a8ff
Aiden Scott <aiden.scott@ovoenergy.com>
2021-07-19 22:51:44 +0100

    henry

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,22 @@ import { Circle } from "./shapes/Circle";
 import { Wiggler } from "./shapes/Wiggler";
 import './style.css';
 import { KeyPresser } from "./utils/KeyPresser";
+import henry from './images/henry.gif'
+
 
 const App = () => (
   <>
     <KeyPresser
       mappings={[
         { key: "a", component: WrappedAtom },
+        { key: "h", component:  Henry},
         { key: "b", component: Circle },
         { key: "w", component: Wiggler },
-        // { key: "c", component:s ConfettiCircle },
       ]} />
   </>
 );
 
 export default App;
+
+
+export const Henry =  () => <img alt='' src={henry}/>
